49d87b99f8ae096f486cb4f2b10980f1ccadf364
Themba <mndebele.themba@gmail.com>
2025-01-20 22:48:37 +0200

    fix cors

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,10 @@ const nextConfig: NextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://nxt-gamma.vercel.app",
+          },
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
