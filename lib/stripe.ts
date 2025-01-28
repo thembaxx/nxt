243108119b981ae3b68e7ed6be5c55f51c77054d
Themba <mndebele.themba@gmail.com>
2025-01-28 22:17:28 +0200

    fix

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2024-12-18.acacia",
+  // apiVersion: "2025-01-27.acacia",
   appInfo: {
     name: "NXT",
     url: "https://nxt-gamma.vercel.app/",
